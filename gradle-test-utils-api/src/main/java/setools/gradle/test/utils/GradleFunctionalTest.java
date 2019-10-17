10e3a6fc4015e1402e838506d27779a2a26d4190
M. Fischer <mfisch2011@gmail.com>
2019-10-17 15:09:01 -0400

    ...

--- gradle-test-utils-api/src/main/java/setools/gradle/test/utils/GradleFunctionalTest.java
+++ gradle-test-utils-api/src/main/java/setools/gradle/test/utils/GradleFunctionalTest.java
@@ -3,6 +3,17 @@
  */
 package setools.gradle.test.utils;
 
+import java.io.File;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Set;
+import java.util.Collection;
+import java.util.HashSet;
+
+import org.gradle.internal.impldep.org.apache.commons.io.FileUtils;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
@@ -27,7 +38,58 @@ public class GradleFunctionalTest {
 		testProjectDir.delete();
 	}
 	
-	protected void copyProjectResources(String path) {
-		//TODO:stub...
+	protected void copyProjectResources(String path) throws MalformedURLException {
+		for(URL resource : getResourcesRecursively(path))
+			copyResourceWithPath(path,resource,testProjectDir.getRoot());
+	}
+
+	protected void copyResourceWithPath(String path,URL resource, File root) {
+		Path resourcePath = relativize(path,resource.getPath());
+		//TODO:create all parent directories...
+		//Path targetPath = root.toPath().resolve(resourcePath);
+		//InputStream stream = resource.openStream();
+		//FileUtils.copyInputStreamToFile(stream,targetPath.toFile());
+		//stream.close();
+	}
+
+	protected Path relativize(String root,String path) {
+		// TODO Auto-generated method stub
+		System.out.printf("RELITIVIZE PATH: %s with %s%n", path,root);
+		return null;
+	}
+
+	protected Set<URL> getResourcesRecursively(String path) throws MalformedURLException {
+		Set<URL> results = new HashSet<URL>();
+		ClassLoader classloader = getClass().getClassLoader();
+		String basePath = getClass().getPackage().getName().replace(".", "/") + "/" + path;
+		URL baseRoot = classloader.getResource(basePath + "/build.gradle");
+		if(baseRoot!=null) {
+			String protocol = baseRoot.getProtocol();
+			if(protocol.startsWith("jar:")) {
+				//handle resources inside jar files
+			} else if(protocol.startsWith("file:")) {
+				File dir = new File(baseRoot.getPath()).getParentFile();
+				results.addAll(convertFilesToUrl(baseRoot,search(dir)));
+			}
+		}
+		return results;
+	}
+	
+	protected static Set<URL> convertFilesToUrl(URL baseRoot, Set<File> files) throws MalformedURLException {
+		Set<URL> results = new HashSet<URL>();
+		for(File file : files)
+			results.add(new URL(baseRoot,file.getPath()));
+		return results;
+	}
+
+	public static Set<File> search(File root) {
+		Set<File> results = new HashSet<File>();
+		for(File file : root.listFiles()) {
+			if(file.isDirectory())
+				results.addAll(search(file));
+			else
+				results.add(file);
+		}
+		return results;
 	}
 }
