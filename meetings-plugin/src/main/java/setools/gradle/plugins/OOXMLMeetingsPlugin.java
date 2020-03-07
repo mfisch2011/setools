315c527f897561a4398ea4134106922ddac9b18e
M. Fischer <mfisch2011@gmail.com>
2020-03-07 09:51:35 -0500

    Overrode task descriptions.
    Changed generic task descriptions to indicate their ooxml format and
    function.

--- meetings-plugin/src/main/java/setools/gradle/plugins/OOXMLMeetingsPlugin.java
+++ meetings-plugin/src/main/java/setools/gradle/plugins/OOXMLMeetingsPlugin.java
@@ -15,6 +15,9 @@
 */
 package setools.gradle.plugins;
 
+import org.gradle.api.Project;
+import org.gradle.api.Task;
+
 import setools.gradle.tasks.OOXMLAssemblePresentation;
 import setools.gradle.tasks.OOXMLDraftAgenda;
 import setools.gradle.tasks.OOXMLDraftMinutes;
@@ -28,5 +31,33 @@ public class OOXMLMeetingsPlugin extends AbstractMeetingsPlugin {
 
 	public OOXMLMeetingsPlugin() {
 		super(OOXMLDraftAgenda.class,OOXMLDraftMinutes.class,OOXMLDraftPresentation.class, OOXMLAssemblePresentation.class,null,null);
+	}
+
+	@Override
+	protected Task configureAssemblePresentation(Project project) {
+		Task task = super.configureAssemblePresentation(project);
+		task.setDescription("Assembles slides into the final pptx presentation.");
+		return task;
+	}
+
+	@Override
+	protected Task configureDraftPresentation(Project project) {
+		Task task = super.configureDraftPresentation(project);
+		task.setDescription("Generates draft pptx slides from the meeting agenda.");
+		return task;
+	}
+
+	@Override
+	protected Task configureDraftMinutes(Project project) {
+		Task task = super.configureDraftMinutes(project);
+		task.setDescription("Generates draft docx minutes from the meeting agenda.");
+		return task;
+	}
+
+	@Override
+	protected Task configureDraftAgenda(Project project) {
+		Task task = super.configureDraftAgenda(project);
+		task.setDescription("Generates draft docx agenda from the meeting agenda.");
+		return task;
 	}
 }
