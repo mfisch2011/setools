5e485394c1a15a3f623036853bd052b60b27ed55
M. Fischer <mfisch2011@gmail.com>
2021-03-17 21:03:26 -0400

    Added MeetingTest abstract unit test

---
+++ api/meetings-base-plugin/src/test/java/setools/gradle/meetings/dsl/MeetingTest.java
@@ -0,0 +1,85 @@
+/**
+ * Copyright 2021 M. Fischer <mfisch2011@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package setools.gradle.meetings.dsl;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+/**
+ * TODO:documentation...
+ */
+public abstract class MeetingTest {
+
+	/**
+	 * Test method for {@link setools.gradle.meetings.dsl.Meeting#getAgendaTemplate()}
+	 * and {@link setools.gradle.meetings.dsl.Meeting#setAgendaTemplate(java.lang.Object)}.
+	 */
+	@Disabled
+	@Test
+	void testGetSetAgendaTemplate() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link setools.gradle.meetings.dsl.Meeting#getMinutesTemplate()}
+	 * and {@link setools.gradle.meetings.dsl.Meeting#setMinutesTemplate(java.lang.Object)}.
+	 */
+	@Disabled
+	@Test
+	void testGetSetMinutesTemplate() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link setools.gradle.meetings.dsl.Meeting#getPresentationTemplate()}
+	 * and {@link setools.gradle.meetings.dsl.Meeting#setPresentationTemplate(java.lang.Object)}.
+	 */
+	@Disabled
+	@Test
+	void testGetSetPresentationTemplate() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link setools.gradle.meetings.dsl.Meeting#agenda()}.
+	 */
+	@Disabled
+	@Test
+	void testAgenda() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link setools.gradle.meetings.dsl.Meeting#agenda(groovy.lang.Closure)}.
+	 */
+	@Disabled
+	@Test
+	void testAgendaClosure() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link setools.gradle.meetings.dsl.Meeting#agenda(org.gradle.api.Action)}.
+	 */
+	@Disabled
+	@Test
+	void testAgendaActionOfQextendsAgendaHandler() {
+		fail("Not yet implemented");
+	}
+
+}
