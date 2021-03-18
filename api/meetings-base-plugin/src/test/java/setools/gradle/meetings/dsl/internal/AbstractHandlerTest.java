c6c98fc582690ffa5bec1bc70cd561e97c0ad173
M. Fischer <mfisch2011@gmail.com>
2021-03-17 21:53:57 -0400

    Added AbstractHandlerTest abstract unit test

---
+++ api/meetings-base-plugin/src/test/java/setools/gradle/meetings/dsl/internal/AbstractHandlerTest.java
@@ -0,0 +1,159 @@
+/**
+ * Copyright 2021 M. Fischer <mfisch2011@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package setools.gradle.meetings.dsl.internal;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Collection;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+/**
+ * TODO:documentation...
+ */
+public abstract class AbstractHandlerTest<T> {
+
+	/**
+	 * TODO:documentation...
+	 * @return
+	 */
+	protected abstract T newObjectInstance();
+	
+	/**
+	 * TODO:documentation...
+	 * @return
+	 */
+	protected abstract Collection<T> newHandlerInstance();
+	
+	/**
+	 * Test method for {@link setools.gradle.meetings.dsl.internal.AbstractHandler#size()}.
+	 */
+	@Disabled
+	@Test
+	void testSize() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link setools.gradle.meetings.dsl.internal.AbstractHandler#isEmpty()}.
+	 */
+	@Disabled
+	@Test
+	void testIsEmpty() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link setools.gradle.meetings.dsl.internal.AbstractHandler#contains(java.lang.Object)}.
+	 */
+	@Disabled
+	@Test
+	void testContains() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link setools.gradle.meetings.dsl.internal.AbstractHandler#iterator()}.
+	 */
+	@Disabled
+	@Test
+	void testIterator() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link setools.gradle.meetings.dsl.internal.AbstractHandler#toArray()}.
+	 */
+	@Disabled
+	@Test
+	void testToArray() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link setools.gradle.meetings.dsl.internal.AbstractHandler#toArray(T[])}.
+	 */
+	@Disabled
+	@Test
+	void testToArrayTArray() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link setools.gradle.meetings.dsl.internal.AbstractHandler#add(java.lang.Object)}.
+	 */
+	@Disabled
+	@Test
+	void testAdd() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link setools.gradle.meetings.dsl.internal.AbstractHandler#remove(java.lang.Object)}.
+	 */
+	@Disabled
+	@Test
+	void testRemove() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link setools.gradle.meetings.dsl.internal.AbstractHandler#containsAll(java.util.Collection)}.
+	 */
+	@Disabled
+	@Test
+	void testContainsAll() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link setools.gradle.meetings.dsl.internal.AbstractHandler#addAll(java.util.Collection)}.
+	 */
+	@Disabled
+	@Test
+	void testAddAll() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link setools.gradle.meetings.dsl.internal.AbstractHandler#removeAll(java.util.Collection)}.
+	 */
+	@Disabled
+	@Test
+	void testRemoveAll() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link setools.gradle.meetings.dsl.internal.AbstractHandler#retainAll(java.util.Collection)}.
+	 */
+	@Disabled
+	@Test
+	void testRetainAll() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link setools.gradle.meetings.dsl.internal.AbstractHandler#clear()}.
+	 */
+	@Disabled
+	@Test
+	void testClear() {
+		fail("Not yet implemented");
+	}
+
+}
