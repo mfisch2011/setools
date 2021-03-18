7610ea0ae6d9f877b3d543b06b0bc9166e25ae12
M. Fischer <mfisch2011@gmail.com>
2021-03-17 21:23:43 -0400

    Added AttendeesHandlerTest abstract unit test

---
+++ api/meetings-base-plugin/src/test/java/setools/gradle/meetings/dsl/AttendeesHandlerTest.java
@@ -0,0 +1,226 @@
+/**
+ * Copyright 2021 M. Fischer <mfisch2011@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package setools.gradle.meetings.dsl;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+/**
+ * TODO:documentation...
+ */
+class AttendeesHandlerTest {
+
+	/**
+	 * Test method for {@link setools.gradle.meetings.dsl.AttendeesHandler#attendee()}.
+	 */
+	@Disabled
+	@Test
+	void testAttendee() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link setools.gradle.meetings.dsl.AttendeesHandler#attendee(groovy.lang.Closure)}.
+	 */
+	@Disabled
+	@Test
+	void testAttendeeClosure() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link setools.gradle.meetings.dsl.AttendeesHandler#attendee(org.gradle.api.Action)}.
+	 */
+	@Disabled
+	@Test
+	void testAttendeeActionOfQextendsAttendee() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.util.Collection#iterator()}.
+	 */
+	@Disabled
+	@Test
+	void testIterator() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.util.Collection#spliterator()}.
+	 */
+	@Disabled
+	@Test
+	void testSpliterator() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.util.Collection#size()}.
+	 */
+	@Disabled
+	@Test
+	void testSize() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.util.Collection#isEmpty()}.
+	 */
+	@Disabled
+	@Test
+	void testIsEmpty() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.util.Collection#contains(java.lang.Object)}.
+	 */
+	@Disabled
+	@Test
+	void testContains() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.util.Collection#toArray()}.
+	 */
+	@Disabled
+	@Test
+	void testToArray() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.util.Collection#toArray(T[])}.
+	 */
+	@Disabled
+	@Test
+	void testToArrayTArray() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.util.Collection#toArray(java.util.function.IntFunction)}.
+	 */
+	@Disabled
+	@Test
+	void testToArrayIntFunctionOfT() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.util.Collection#add(java.lang.Object)}.
+	 */
+	@Disabled
+	@Test
+	void testAdd() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.util.Collection#remove(java.lang.Object)}.
+	 */
+	@Disabled
+	@Test
+	void testRemove() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.util.Collection#containsAll(java.util.Collection)}.
+	 */
+	@Disabled
+	@Test
+	void testContainsAll() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.util.Collection#addAll(java.util.Collection)}.
+	 */
+	@Disabled
+	@Test
+	void testAddAll() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.util.Collection#removeAll(java.util.Collection)}.
+	 */
+	@Disabled
+	@Test
+	void testRemoveAll() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.util.Collection#removeIf(java.util.function.Predicate)}.
+	 */
+	@Disabled
+	@Test
+	void testRemoveIf() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.util.Collection#retainAll(java.util.Collection)}.
+	 */
+	@Disabled
+	@Test
+	void testRetainAll() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.util.Collection#clear()}.
+	 */
+	@Disabled
+	@Test
+	void testClear() {
+		fail("Not yet implemented");
+	}
+	
+	/**
+	 * Test method for {@link java.util.Collection#stream()}.
+	 */
+	@Disabled
+	@Test
+	void testStream() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.util.Collection#parallelStream()}.
+	 */
+	@Disabled
+	@Test
+	void testParallelStream() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.lang.Iterable#forEach(java.util.function.Consumer)}.
+	 */
+	@Disabled
+	@Test
+	void testForEach() {
+		fail("Not yet implemented");
+	}
+
+}
