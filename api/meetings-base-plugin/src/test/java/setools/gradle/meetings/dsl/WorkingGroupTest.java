dba13d64063d6255d6155c5197b629b590119eb4
M. Fischer <mfisch2011@gmail.com>
2021-03-17 21:14:27 -0400

    Added WorkingGroupTest abstract unit test

---
+++ api/meetings-base-plugin/src/test/java/setools/gradle/meetings/dsl/WorkingGroupTest.java
@@ -0,0 +1,38 @@
+/**
+ * Copyright 2021 M. Fischer <mfisch2011@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package setools.gradle.meetings.dsl;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+/**
+ * TODO:documentation...
+ */
+class WorkingGroupTest extends MeetingTest {
+
+	/**
+	 * Test method for {@link setools.gradle.meetings.dsl.WorkingGroup#getActionItemDatabase()}
+	 * and {@link setools.gradle.meetings.dsl.WorkingGroup#setActionItemDatabase(java.lang.String)}.
+	 */
+	@Disabled
+	@Test
+	void testGetSetActionItemDatabase() {
+		fail("Not yet implemented");
+	}
+
+}
