6c5e341ff377e7f6cd270deb1b8e119f24796c8a
M. Fischer <mfisch2011@gmail.com>
2021-05-03 12:35:13 -0400

    Fixed GradleTestRunner to properly handle exceptions

--- api/functional-test-api/src/main/java/setools/gradle/test/GradleTestRunner.java
+++ api/functional-test-api/src/main/java/setools/gradle/test/GradleTestRunner.java
@@ -7,6 +7,7 @@ import java.io.File;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.net.URL;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.util.Arrays;
@@ -15,6 +16,7 @@ import org.gradle.internal.impldep.org.apache.commons.io.FileUtils;
 import org.junit.Ignore;
 import org.junit.runner.Description;
 import org.junit.runner.Runner;
+import org.junit.runner.notification.Failure;
 import org.junit.runner.notification.RunNotifier;
 import org.gradle.testkit.runner.BuildResult;
 import org.gradle.testkit.runner.GradleRunner;
@@ -46,25 +48,40 @@ public class GradleTestRunner extends Runner {
 	@Override
 	public void run(RunNotifier notifier) {
 		System.out.println("Running the GradleTests from: " + testClass);
-		try {
-			Object testObject = testClass.getConstructor().newInstance();
-			for(Method method : testClass.getMethods()) {
-				if(!method.isAnnotationPresent(Ignore.class)) {
-					GradleTest annotation = method.getAnnotation(GradleTest.class);
-					if(annotation!=null) {
-						File tmpDir = initializeTest(annotation);
-						invokeTest(notifier,method,testObject,tmpDir,annotation);
-						tmpDir.delete();
-					}
-				} else {
-					notifier.fireTestIgnored(Description
-					.createTestDescription(testClass, method.getName()));
+		for(Method method : testClass.getMethods()) {
+			GradleTest annotation = method.getAnnotation(GradleTest.class);
+			Ignore ignore = method.getAnnotation(Ignore.class);
+			Description desc = Description.createTestDescription(
+					testClass, method.getName());
+			try {
+				if(annotation!=null && ignore==null) {
+					executeTest(notifier,method,annotation,desc);
+				} else if(annotation!=null && ignore!=null) {
+					notifier.fireTestIgnored(desc);
 				}
+			} catch(Exception e) {
+				Failure failure = new Failure(desc,e);
+				notifier.fireTestFailure(failure);
 			}
-		} catch (Exception e) {
-			//TODO: notify failure???
-			throw new RuntimeException(e);
 		}
+	}
+
+	/**
+	 * TODO:documentation...
+	 * @param notifier
+	 * @param method
+	 * @param testObject
+	 * @param annotation
+	 * @param desc
+	 */
+	protected void executeTest(RunNotifier notifier,
+			Method method,
+			GradleTest annotation,
+			Description desc) throws Exception {
+		Object testObject = testClass.getConstructor().newInstance();
+		File tmpDir = initializeTest(annotation);
+		invokeTest(notifier,method,testObject,tmpDir,annotation);
+		tmpDir.delete();
 	}
 
 	/**
@@ -108,7 +125,8 @@ public class GradleTestRunner extends Runner {
 		//copy resources so that we can override from other attributes
 		if(!isNullOrEmpty(annotation.resources())) {
 			//TODO:add ability to copy from jar location...
-			File srcDir = new File(annotation.resources());
+			URL url = testClass.getResource(annotation.resources());
+			File srcDir = new File(url.getPath());
 			FileUtils.copyDirectory(srcDir, dir);
 		}
 		
@@ -143,5 +161,22 @@ public class GradleTestRunner extends Runner {
 	 */
 	protected static boolean isNullOrEmpty(String text) {
 		return text==null || text.isEmpty();
+	}
+	
+	/**
+	 * TODO:documentation...
+	 * @param dir
+	 */
+	public static void listFiles(File dir) {
+		System.out.println(dir);
+		File[] files = dir.listFiles();
+		if(files!=null) {
+			for(File file : files) {
+				if(file.isDirectory())
+					listFiles(file);
+				else
+					System.out.println(file);
+			}
+		}
 	}
 }
