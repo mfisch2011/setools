da65a2007311e0ced7c09564b287b6eb94c08627
M. Fischer <mfisch2011@gmail.com>
2022-02-27 18:06:47 -0500

    TCR!!!!

--- plugins/meetings-plugin/src/test/java/setools/gradle/meetings/plugins/MeetingsPluginTest.java
+++ plugins/meetings-plugin/src/test/java/setools/gradle/meetings/plugins/MeetingsPluginTest.java
@@ -91,7 +91,7 @@ public class MeetingsPluginTest {
 		writeFile(settingsFile, "rootProject.name = 'test-project'");
 		String text = getPluginConfig();
 		text += "meetings {" + System.lineSeparator()
-			+ "  meetings" + System.lineSeparator()
+			+ "  meeting()" + System.lineSeparator()
 			+ "}";
 		writeFile(buildFile,text);
 		BuildResult result = GradleRunner.create()
