398601300e53aa074359a1569aba1220a74214b0
M. Fischer <mfisch2011@gmail.com>
2022-02-27 12:48:20 -0500

    TCR Commit

---
+++ plugins/meetings-source-set/src/test/java/setools/gradle/meetings/tasks/internal/DefaultMeetingSourceDirectorySetTest.java
@@ -0,0 +1,73 @@
+/**
+ * Copyright 2022 M. Fischer <mfisch2011@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package setools.gradle.meetings.tasks.internal;
+
+import static org.junit.Assert.*;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+/**
+ * TODO:
+ */
+public class DefaultMeetingSourceDirectorySetTest {
+
+	/**
+	 * Test method for {@link setools.gradle.meetings.tasks.internal.DefaultMeetingSourceDirectorySet#DefaultMeetingSourceDirectorySet(java.lang.String, org.gradle.api.model.ObjectFactory)}.
+	 */
+	@Test
+	@Ignore
+	public void testDefaultMeetingSourceDirectorySet() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link setools.gradle.meetings.tasks.internal.DefaultMeetingSourceDirectorySet#getAgenda()}.
+	 */
+	@Test
+	@Ignore
+	public void testGetAgenda() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link setools.gradle.meetings.tasks.internal.DefaultMeetingSourceDirectorySet#getMinutes()}.
+	 */
+	@Test
+	@Ignore
+	public void testGetMinutes() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link setools.gradle.meetings.tasks.internal.DefaultMeetingSourceDirectorySet#getPresentation()}.
+	 */
+	@Test
+	@Ignore
+	public void testGetPresentation() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link setools.gradle.meetings.tasks.internal.DefaultMeetingSourceDirectorySet#getAllSources()}.
+	 */
+	@Test
+	@Ignore
+	public void testGetAllSources() {
+		fail("Not yet implemented");
+	}
+
+}
