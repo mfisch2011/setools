8730093ea0027729933c9cfbbb83f2bb9831c927
M. Fischer <mfisch2011@gmail.com>
2022-02-27 17:49:27 -0500

    TCR Commit

--- plugins/meetings-plugin/src/main/java/setools/gradle/meetings/plugins/internal/DefaultMeetingsFactory.java
+++ plugins/meetings-plugin/src/main/java/setools/gradle/meetings/plugins/internal/DefaultMeetingsFactory.java
@@ -15,62 +15,94 @@
  */
 package setools.gradle.meetings.plugins.internal;
 
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
 import org.gradle.api.Project;
-import org.gradle.api.logging.Logger;
 import org.gradle.internal.metaobject.DynamicInvokeResult;
 
+import setools.gradle.meetings.api.Meeting;
 import setools.gradle.meetings.api.MeetingsFactory;
+import setools.gradle.meetings.plugins.MeetingsPluginExt;
 
 /**
  * TODO:
  */
 public class DefaultMeetingsFactory implements MeetingsFactory {
-
+	
 	/**
 	 * TODO:
 	 */
-	protected final Map<String,Object> handlers;
+	protected final ArrayList<Object> handlers =
+			new ArrayList<Object>();
 	
 	/**
 	 * TODO:
 	 */
 	private Project project = null;
-
-	/**
-	 * TODO:
-	 */
-	private Logger logger;
-	
-	
-	/**
-	 * TODO:
-	 */
-	public DefaultMeetingsFactory() {
-		this.handlers = new ConcurrentHashMap<String,Object>();
-	}
 	
 	@Override
 	public void apply(Project project) {
 		this.project = project;
-		this.logger = project.getLogger();
 		//TODO anything to do here ???
 	}
 
 	@Override
 	public boolean hasMethod(String name, Object...args) {
-		logger.lifecycle("Searching for method '{}' with arguments {}.",name,args);
-		// TODO Auto-generated method stub
+		Class<?>[] params = new Class<?>[args.length];
+		for(int x=0;x<args.length;x++) {
+			params[x] = args[x].getClass();
+		}
+		
+		for(Object handler : handlers) {
+			Method method;
+			try {
+				method = handler.getClass().getMethod(name, params);
+				if(Meeting.class.isAssignableFrom(method.getReturnType()))
+					return true;
+			} catch (NoSuchMethodException | SecurityException e) {
+				//ignore expected
+			}
+		}
 		return false;
 	}
 
 	@Override
 	public DynamicInvokeResult tryInvokeMethod(String name, Object...args) {
-		logger.lifecycle("I method '{}' with arguments {}.",name,args);
-		// TODO Auto-generated method stub
+		Class<?>[] params = new Class<?>[args.length];
+		for(int x=0;x<args.length;x++) {
+			params[x] = args[x].getClass();
+		}
+		
+		for(Object handler : handlers) {
+			Method method;
+			try {
+				method = handler.getClass().getMethod(name, params);
+				if(Meeting.class.isAssignableFrom(method.getReturnType())) {
+					Object result = method.invoke(handler, args);
+					onSuccess(Meeting.class.cast(result));
+				}
+			} catch (NoSuchMethodException | SecurityException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+				//ignore expected
+			}
+		}
 		return DynamicInvokeResult.notFound();
 	}
 
+	/**
+	 * TODO:
+	 * @param cast
+	 */
+	protected void onSuccess(Meeting meeting) {
+		getMeetings().add(meeting);
+	}
+
+	/**
+	 * TODO:
+	 * @return
+	 */
+	protected MeetingsPluginExt getMeetings() {
+		return (MeetingsPluginExt)project.getPluginManager()
+			.findPlugin(MeetingsPluginExt.MEETINGS_EXT_NAME);
+	}
 }
