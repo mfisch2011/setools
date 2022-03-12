e7ab8a05b29c176841e1bc783aca9e5dcdc81e4e
M. Fischer <mfisch2011@gmail.com>
2022-03-12 11:44:16 -0500

    # WARNING: head commit changed in the meantime
    TCR...

---
+++ api/gradle-utils/src/main/java/setools/gradle/plugins/AbstractSourceSetPlugin.java
@@ -0,0 +1,156 @@
+/**
+ * Copyright 2022 M. Fischer <mfisch2011@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package setools.gradle.plugins;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import javax.inject.Inject;
+
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+import org.gradle.api.internal.tasks.DefaultSourceSet;
+import org.gradle.api.plugins.JavaPluginExtension;
+import org.gradle.api.plugins.JvmEcosystemPlugin;
+import org.gradle.api.tasks.SourceSet;
+import org.gradle.api.tasks.SourceSetContainer;
+import org.gradle.internal.reflect.Instantiator;
+
+import setools.gradle.tasks.SourceSetExtensionConfigAction;
+
+/**
+ * TODO:
+ */
+public class AbstractSourceSetPlugin implements Plugin<Project> {
+	
+	/**
+	 * {@link Class} with type of source set to create.
+	 */
+	protected final Class<? super SourceSet> sourceSetType;
+	
+	/**
+	 * Extensions to create for source set(s).
+	 */
+	protected final Map<String,Class<?>> extensions =
+		new HashMap<String,Class<?>>();
+
+	/**
+	 * Name of new {@link SourceSet} to create.
+	 */
+	protected final String sourceSetName;
+
+	/**
+	 * Newly created {@link SourceSet}.
+	 */
+	private SourceSet sourceSet = null;
+
+	/**
+	 * {@link Instantiator} used to create new {@link SourceSet}s and 
+	 * inject parameters at construction.
+	 */
+	protected final Instantiator instantiator;
+	
+	/**
+	 * Get the newly created SourceSet.
+	 * @return - {@link SourceSet} created
+	 */
+	protected SourceSet getSourceSet() {
+		return sourceSet;
+	}
+
+	/**
+	 * Constructor for an AbstractSourceSetPlugin.
+	 * 
+	 * @param type - {@link Class} of {@link SourceSet} to create.  null 
+	 * will create {@link DefaultSourceSet}.
+	 * 
+	 * @param name - {@link String} with name of the new {@link SourceSet}
+	 * @param instantiator - {@link Instantiator} used to create the new {@link SourceSet}
+	 */
+	@Inject
+	public AbstractSourceSetPlugin(Class<? super SourceSet> type,String name,Instantiator instantiator) {
+		this.sourceSetType = type;
+		this.sourceSetName = name;
+		this.instantiator = instantiator;
+	}
+	
+	/**
+	 * TODO:
+	 * @param sourceSets - {@link SourceSetContainer} to add {@link SourceSet} to
+	 * @param type - {@link Class} of {@link SourceSet} to create
+	 * @param name - {@link String} with name of new {@link SourceSet}
+	 * @return {@link SourceSet} created
+	 */
+	protected SourceSet addSourceSet(SourceSetContainer sourceSets, Class<? super SourceSet> type,String name) {
+		if(type==null) {
+			return sourceSets.create(name);
+		} else {
+			SourceSet sourceSet = (SourceSet) instantiator.newInstance(type, name);
+			sourceSets.add(sourceSet);
+			//TODO:configure classes or leave that to the SourceSet ????
+	        return sourceSet;
+		}
+	}
+
+	@Override
+	public void apply(Project project) {
+		project.getPluginManager().apply(JvmEcosystemPlugin.class);
+		SourceSetContainer sourceSets = project.getExtensions()
+				.getByType(JavaPluginExtension.class).getSourceSets();
+		if(sourceSetName!=null) {
+			sourceSet = addSourceSet(sourceSets,sourceSetType,sourceSetName);
+		}
+		
+		//TODO:how to register callbacks to add SourceDirectorySet(s) after all SourceSets are registered ???
+		if(sourceSet==null) {
+			//add SourceDirectorySet(s) to all SourceSets
+			addSourceDirectorySets(sourceSets);
+		} else {
+			//add SourceDirectorySet(s) to only new sourceSet
+			addSourceDirectorySets(sourceSet);
+		}
+	}
+
+	/**
+	 * TODO:
+	 * @param sourceSet - {@link SourceSet} to add extensions to
+	 */
+	protected void addSourceDirectorySets(SourceSet sourceSet) {
+		for(Entry<String, Class<?>> entry : extensions.entrySet()) {
+			String name = entry.getKey();
+			Class<?> extension = entry.getValue();
+			//TODO:do we want to use instantiator to create Action to enable injection ???
+			SourceSetExtensionConfigAction action = new SourceSetExtensionConfigAction(name,extension);
+			action.execute(sourceSet);
+		}
+	}
+
+	/**
+	 * TODO:
+	 * @param sourceSets - {@link SourceSetContainer} with 
+	 * {@link SourceSet}(s) to add extensions to
+	 */
+	protected void addSourceDirectorySets(SourceSetContainer sourceSets) {
+		for(Entry<String, Class<?>> entry : extensions.entrySet()) {
+			String name = entry.getKey();
+			Class<?> extension = entry.getValue();
+			//TODO:do we want to use instantiator to create Action to enable injection ???
+			sourceSets.all(new SourceSetExtensionConfigAction(name,extension));
+		}
+	}
+
+}
---
+++ api/gradle-utils/src/main/java/setools/gradle/tasks/SourceSetExtensionConfigAction.java
@@ -0,0 +1,55 @@
+/**
+ * Copyright 2022 M. Fischer <mfisch2011@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package setools.gradle.tasks;
+
+import org.gradle.api.Action;
+import org.gradle.api.internal.tasks.DefaultSourceSet;
+import org.gradle.api.tasks.SourceSet;
+
+/**
+ * TODO:
+ */
+public class SourceSetExtensionConfigAction implements Action<SourceSet> {
+
+	/**
+	 * The name of the new extension to create and configure.
+	 */
+	protected final String name;
+	
+	/**
+	 * {@link Class} type of the new extension to create and configure.
+	 */
+	protected final Class<?> type;
+
+	/**
+	 * Create and configure a new extension with name and type.
+	 * 
+	 * @param name - name for the extension
+	 * @param type - {@link Class} of extension to create
+	 */
+	public SourceSetExtensionConfigAction(String name, Class<?> type) {
+		this.name = name;
+		this.type = type;
+	}
+
+	@Override
+	public void execute(SourceSet sourceSet) {
+		String displayName = ((DefaultSourceSet) sourceSet).getDisplayName();
+		sourceSet.getExtensions().create(name, type,displayName);
+		//TODO:other configuration ???
+	}
+
+}
