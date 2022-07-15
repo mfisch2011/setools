2bd64afaecca35a6e3010c41cb0511f603fae03b
M. Fischer <mfisch2011@gmail.com>
2022-07-14 21:33:33 -0400

    Debugging TestCommitRevertClosure...

--- buildSrc/src/main/java/com/github/mfisch2011/gradle/TestCommitRevertClosure.java
+++ buildSrc/src/main/java/com/github/mfisch2011/gradle/TestCommitRevertClosure.java
@@ -29,6 +29,7 @@ import org.eclipse.jgit.revwalk.RevWalk;
 import org.eclipse.jgit.revwalk.RevCommit;
 import org.eclipse.jgit.lib.Repository;
 import org.eclipse.jgit.lib.Constants;
+import org.eclipse.jgit.treewalk.TreeWalk;
 
 import groovy.lang.Closure;
 
@@ -67,6 +68,14 @@ public class TestCommitRevertClosure extends Closure<Object> {
 		RevCommit head = walker.parseCommit(repo.resolve(Constants.HEAD));
 		git.revert().include(head).call();
 		getProject().getLogger().lifecycle("Reverted changes to {}.",head);
+		TreeWalk tree = new TreeWalk(repo);
+		tree.addTree(head.getTree());
+		tree.setRecursive(true);
+		String msg = "Reverted: ";
+		while(tree.next()) {
+			msg += tree.getPathString() + System.lineSeparator();
+		}
+		getProject().getLogger().lifecycle(msg);
 	}
 	
 	/**
@@ -91,6 +100,15 @@ public class TestCommitRevertClosure extends Closure<Object> {
 		// and then commit the changes.
 		RevCommit commit = git.commit().setMessage(commit_message()).call();
 		getProject().getLogger().lifecycle("Commited changes.  New commit {}.",commit);
+		Repository repo = git.getRepository();
+		TreeWalk tree = new TreeWalk(repo);
+		tree.addTree(commit.getTree());
+		tree.setRecursive(true);
+		String msg = "Committed: ";
+		while(tree.next()) {
+			msg += tree.getPathString() + System.lineSeparator();
+		}
+		getProject().getLogger().lifecycle(msg);
 	}
 	
 	/**
